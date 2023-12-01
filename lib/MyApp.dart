db47ced987ff1918ed230f7cce8a92898e751d3a
carlosgr1992 <carlos.gr1992@gmail.com>
2023-12-01 09:01:31 +0100

    PMDM: Creacion de la clase MyApp y refactorizado codigo para hacer el run de MyApp

---
+++ lib/MyApp.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "MyApp",);
+  }
+}
