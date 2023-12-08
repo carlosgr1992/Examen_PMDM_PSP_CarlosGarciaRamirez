7b348215deab6657be3bb6c5a8f81ce409aac386
carlosgr1992 <carlos.gr1992@gmail.com>
2023-12-08 21:07:59 +0100

    PMDM 5: Agregado un floatingButton en el home que añade y actualiza los usuarios del home

--- lib/onBoarding/HomeView.dart
+++ lib/onBoarding/HomeView.dart
@@ -1,13 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:examen_pmdm_psp_carlosgarciaramirez/singletone/DataHolder.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../customViews/ButtonBarCustom.dart';
 import '../customViews/DrawerCustom.dart';
 import '../fireStoreObjets/FbUsuario.dart';
-import '../singletone/FireBaseAdmin.dart';
 import 'AjustesView.dart';
 import 'UsuarioDetailsView.dart';
 
@@ -19,13 +17,11 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   bool isList = true;
   FbUsuario? currentUser;
-  Map<String, FbUsuario> usuariosMap = {}; // Nuevo mapa para usuarios y sus UIDs
 
   @override
   void initState() {
     super.initState();
     _loadCurrentUser();
-    _loadUsuarios(); // Llamar a la nueva función para cargar usuarios
   }
 
   void _loadCurrentUser() async {
@@ -37,18 +33,6 @@ class _HomeViewState extends State<HomeView> {
     }
   }
 
-  void _loadUsuarios() async {
-    // Lógica para cargar usuarios y sus uids
-    var snapshot = await FirebaseFirestore.instance.collection('Usuarios').get();
-    var usuariosTemp = <String, FbUsuario>{};
-    for (var doc in snapshot.docs) {
-      usuariosTemp[doc.id] = FbUsuario.fromFirestore(doc);
-    }
-    setState(() {
-      usuariosMap = usuariosTemp;
-    });
-  }
-
   void _navigateToAjustesView(String uid, FbUsuario usuario) {
     Navigator.of(context).push(
       MaterialPageRoute(
@@ -57,51 +41,52 @@ class _HomeViewState extends State<HomeView> {
     );
   }
 
-  Widget muestraListView(List<FbUsuario> usuarios) {
-    return ListView.builder(
-      itemCount: usuarios.length,
-      itemBuilder: (context, index) {
-        FbUsuario usuario = usuarios[index];
-        String uid = usuariosMap.keys.firstWhere((key) => usuariosMap[key] == usuario, orElse: () => '');
-        return ListTile(
-          title: Text('${usuario.nombre} ${usuario.apellidos} - Edad: ${usuario.edad}'),
-          onTap: () => _navigateToAjustesView(uid, usuario),
-        );
-      },
-    );
-  }
+  void _showAddUserDialog() {
+    String nombre = '';
+    String apellidos = '';
+    int edad = 0;
 
-  Widget muestraGridView(List<FbUsuario> usuarios) {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-      ),
-      itemCount: usuarios.length,
-      itemBuilder: (context, index) {
-        FbUsuario usuario = usuarios[index];
-        String uid = usuariosMap.keys.firstWhere((key) => usuariosMap[key] == usuario, orElse: () => '');
-        return GestureDetector(
-          onTap: () => _navigateToAjustesView(uid, usuario),
-          child: Card(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                usuario.urlImagen != null && usuario.urlImagen!.isNotEmpty
-                    ? Image.network(
-                  usuario.urlImagen!,
-                  width: 80,
-                  height: 80,
-                  fit: BoxFit.cover,
-                )
-                    : Icon(Icons.person, size: 80),
-                SizedBox(height: 10),
-                Text('${usuario.nombre} ${usuario.apellidos}'),
-                Text('Edad: ${usuario.edad}'),
-              ],
-            ),
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Añadir nuevo usuario'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextField(
+                onChanged: (value) => nombre = value,
+                decoration: InputDecoration(labelText: 'Nombre'),
+              ),
+              TextField(
+                onChanged: (value) => apellidos = value,
+                decoration: InputDecoration(labelText: 'Apellidos'),
+              ),
+              TextField(
+                onChanged: (value) => edad = int.tryParse(value) ?? 0,
+                decoration: InputDecoration(labelText: 'Edad'),
+                keyboardType: TextInputType.number,
+              ),
+            ],
           ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancelar'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            TextButton(
+              child: Text('Añadir'),
+              onPressed: () {
+                FirebaseFirestore.instance.collection('Usuarios').add({
+                  'nombre': nombre,
+                  'apellidos': apellidos,
+                  'edad': edad,
+                  'urlImagen': '', // Dejar en blanco o poner una URL por defecto
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
         );
       },
     );
@@ -156,7 +141,62 @@ class _HomeViewState extends State<HomeView> {
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showAddUserDialog,
+        child: Icon(Icons.add),
+      ),
     );
   }
 
+  Widget muestraListView(List<FbUsuario> usuarios) {
+    return ListView.builder(
+      itemCount: usuarios.length,
+      itemBuilder: (context, index) {
+        FbUsuario usuario = usuarios[index];
+        return ListTile(
+          title: Text('${usuario.nombre} ${usuario.apellidos} - Edad: ${usuario.edad}'),
+          onTap: () {
+            // Aquí necesitarás encontrar el UID del usuario. Puedes modificar tu enfoque para obtenerlo.
+          },
+        );
+      },
+    );
+  }
+
+  Widget muestraGridView(List<FbUsuario> usuarios) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemCount: usuarios.length,
+      itemBuilder: (context, index) {
+        FbUsuario usuario = usuarios[index];
+        return GestureDetector(
+          onTap: () {
+            // Aquí necesitarás encontrar el UID del usuario. Puedes modificar tu enfoque para obtenerlo.
+          },
+          child: Card(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                usuario.urlImagen != null && usuario.urlImagen!.isNotEmpty
+                    ? Image.network(
+                  usuario.urlImagen!,
+                  width: 80,
+                  height: 80,
+                  fit: BoxFit.cover,
+                )
+                    : Icon(Icons.person, size: 80),
+                SizedBox(height: 10),
+                Text('${usuario.nombre} ${usuario.apellidos}'),
+                Text('Edad: ${usuario.edad}'),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
 }
