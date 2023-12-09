b020fb31b120c25fa86e820ae86620333e94e706
carlosgr1992 <carlos.gr1992@gmail.com>
2023-12-09 20:14:43 +0100

    PSP 5: Agregada la misma imagen del currentUser en el drawer, además de en ajustes y en el propio DetailsView de este usuario, la mostramos en esos 3 sitios diferentes

--- lib/customViews/DrawerCustom.dart
+++ lib/customViews/DrawerCustom.dart
@@ -1,32 +1,42 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import '../fireStoreObjets/FbUsuario.dart';
 import '../onBoarding/AjustesView.dart';
-import '../onBoarding/UsuarioDetailsView.dart';
 
 class DrawerCustom extends StatelessWidget {
   final FbUsuario? currentUser;
-  final String? uid;
 
-  DrawerCustom({required this.currentUser, this.uid});
+  DrawerCustom({this.currentUser});
 
   @override
   Widget build(BuildContext context) {
+    // Verifica si la URL es válida
+    bool esUrlValida = Uri.tryParse(currentUser?.urlImagen ?? '')?.hasAbsolutePath ?? false;
+
     return Drawer(
       child: ListView(
         children: <Widget>[
           DrawerHeader(
-            child: currentUser != null
-                ? Column(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text('Bienvenido, ${currentUser!.nombre}', style: TextStyle(color: Colors.white)),
-                Text('${currentUser!.apellidos}', style: TextStyle(color: Colors.white)),
+                // Mostrar imagen de perfil o ícono por defecto
+                esUrlValida
+                    ? CircleAvatar(
+                  backgroundImage: NetworkImage(currentUser!.urlImagen!),
+                  radius: 40,
+                )
+                    : CircleAvatar(
+                  child: Icon(Icons.person, size: 40),
+                  radius: 40,
+                ),
+                SizedBox(height: 10), // Espacio entre la imagen y el texto
+                Text(
+                  'Bienvenido, ${currentUser?.nombre ?? 'Invitado'}',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
               ],
-            )
-                : Text('Bienvenido, Invitado', style: TextStyle(color: Colors.white)),
+            ),
             decoration: BoxDecoration(
               color: Colors.blue,
             ),
@@ -42,15 +52,11 @@ class DrawerCustom extends StatelessWidget {
             leading: Icon(Icons.settings),
             title: Text('Ajustes'),
             onTap: () {
+              // Navegar a la vista de detalles del usuario
               if (currentUser != null) {
-                Navigator.of(context).pop(); // Cierra el drawer
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => UsuarioDetailsView(usuario: currentUser!, uid: currentUser!.id),
-                  ),
-                );
-              }else {
-
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => AjustesView(usuario: currentUser!, uid: currentUser!.id,),
+                ));
               }
             },
           ),
